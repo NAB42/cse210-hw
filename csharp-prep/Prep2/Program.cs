18a6a5c3fc55d6964771b24ae978424834701c56
NAB42 <fj45cruza@gmail.com>
2026-04-23 21:26:30 -0600

    Created the conditional statments program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,55 @@
-using System;
+// The purpose of this program is to take an integer percentage grade,
 
+using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    {  
+        Console.Write("Enter your course percentage grade: ");
+        string ans = Console.ReadLine();
+        int pct = int.Parse(ans);
+        string letterGrade;
+        //Determines what letter to assign
+        //Honestly a switch case would be better
+        if (pct >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (pct >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (pct >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (pct >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        //Adds the +/- and worms out the exceptions
+        if (!letterGrade.Equals("F"))
+        {
+            int finalNum = int.Parse(ans.Substring(ans.Length-1,ans.Length-1));
+            if (finalNum >= 7)
+            {
+                letterGrade += "+";
+            }
+            else if (finalNum < 3)
+            {
+                letterGrade += "-";
+            } 
+            if (letterGrade.Equals("A+"))
+            {
+                letterGrade = "A";
+            }
+        }
+        Console.WriteLine($"Your final grade is {letterGrade}");
+
     }
 }
