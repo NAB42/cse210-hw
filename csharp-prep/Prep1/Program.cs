9089a447e3c4eb39900a33775d6bd31b22aaf819
NAB42 <fj45cruza@gmail.com>
2026-04-23 20:26:59 -0600

    Fixed a newline error

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What's your first name? ");
+        Console.Write("What's your first name? ");
         string first = Console.ReadLine();
-        Console.WriteLine("What's your last name? ");
+        Console.Write("What's your last name? ");
         string last = Console.ReadLine();
         Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
