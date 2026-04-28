df4c62247d290d1f688b84c088137000d1182bcb
NAB42 <fj45cruza@gmail.com>
2026-04-28 12:44:53 -0600

    Started Prep 4 - Lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,24 @@
 using System;
-
+/* Prep 4 - Lists
+ * Alright, so the purpose of this program is so that I can become more familiar with Lists.
+ * Basically it takes a list of numbers inputted by the user until a 0 is entered. Then it 
+ * makes a sum of the numbers and an average of them, the largest and smallest numbers, and 
+ * a sorted list.
+ */
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int current = 0;
+        do
+        {
+            Console.Write("Enter a number: ");
+            current = int.Parse(Console.ReadLine());
+            numbers.Add(current);
+        }while(current != 0);
+        foreach (int num in numbers){ 
+            Console.Write(num + ", ");
+        }
     }
 }
