f501e65a78508f04b32792a6ec7fe776a733d1a8
NAB42 <fj45cruza@gmail.com>
2026-04-29 14:35:57 -0600

    Completed Prep 4 - Lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -11,19 +11,45 @@ class Program
     {
         List<int> numbers = new List<int>();
         int current = 0;
+        Console.WriteLine("Enter a series of numbers, type 0 when finished");
+
+        // The loop to create the List of numbers
         do
         {
             Console.Write("Enter a number: ");
             current = int.Parse(Console.ReadLine());
             numbers.Add(current);
         }while(current != 0);
+
         int sum = 0;
+        int biggest=0;
+        int smallest = numbers[0];
+
+        // Calculate the sum, average, biggest and smallest numbers.
         foreach (int num in numbers){ 
-            Console.Write(num + ", ");
             sum += num;
+            if(num > biggest)
+            {
+                biggest = num;
+            }
+            if(num < smallest && num != 0)
+            {
+                smallest = num;
+            }
         }
         double avg = (double)sum/(double)numbers.Count()-1;
+
+        // Print out all the results
         Console.WriteLine("Sum is " + sum);
         Console.WriteLine("Avg is " + avg);
+        Console.WriteLine("The biggest number is " + biggest);
+        Console.WriteLine("The smallest number is " + smallest);
+
+        //Sort the list.
+        numbers.Sort();
+        Console.WriteLine("The sorted list is");
+        foreach (int num in numbers){
+            Console.WriteLine(num);
+        }
     }
 }
