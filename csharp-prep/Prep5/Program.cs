7f43c5195b9dfc65605c2322f24253966918895a
NAB42 <fj45cruza@gmail.com>
2026-04-30 10:41:05 -0600

    Completed Prep 5 - Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,47 @@
 using System;
-
+// This is a super redundant display of functions and how they work.
+// The program takes a user's name, favorite number, and birth year, and then calculates
+// accordingly. 
 class Program
 {
+    static void DisplayProgram()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Enter name: ");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Enter favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+    static void PromptUserBirthYear(out int year)
+    {
+        Console.Write("Enter birth year: ");
+        year = int.Parse(Console.ReadLine());
+    }
+    static int Square(int x)
+    {
+        return x*x; 
+    }
+    static void DisplayResult(string name,int number, int year)
+    {
+        Console.WriteLine(name + ", the square of your number is " + Square(number));
+        Console.WriteLine(name + ", you will turn " + (DateTime.Now.Year - year) + " years old this year.");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        int userBirthYear;
+        DisplayProgram();
+        string username = PromptUserName();
+        int number = PromptUserNumber();
+        PromptUserBirthYear(out userBirthYear);
+        DisplayResult(username,number,userBirthYear);
+
+
     }
 }
