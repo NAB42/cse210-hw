6bfa5c2ed189c7dfb2697133680b45fdfaebe17a
NAB42 <fj45cruza@gmail.com>
2026-05-05 09:10:03 -0600

    Running more tests

--- sandbox/CircleApp/Program.cs
+++ sandbox/CircleApp/Program.cs
@@ -3,7 +3,7 @@
     public static void Main(string[] args)
     {
         // This is a test comment for git
-        Console.WriteLine("Hey");
+        Console.WriteLine("Hello");
         Circle c = new Circle(17);
         Console.WriteLine(c.GetArea());
         Cylinder cyl = new Cylinder(c,10);
