aa2b0b77d305b6c51d4641dbc14ad8f9b92a398d
NAB42 <fj45cruza@gmail.com>
2026-05-11 14:42:18 -0600

    Started prompt generation. Currently messy, but it runs!

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -6,12 +6,8 @@ public class Entry
     /* Attributes */
 
     // This is the list of prompts. Currently there are 3.
-    private List<string> _prompts = new List<string>
-    {
-        "How have I seen the hand of the Lord in my life today?",
-        "What is something cool that happened today?",
-        "What is something I am grateful for?"
-    };
+    private List<string> _prompts;
+    private string _promptFilename = "prompts.csv";
     private string _date;
     private string _response;
     private int _count;
@@ -23,6 +19,7 @@ public class Entry
         _date = date;
         _count = count;
         _response = response;
+        this.LoadPrompts();
     }
 
     // This constructor is for when a new Entry is to be created, and there is not yet 
@@ -30,10 +27,75 @@ public class Entry
     public Entry(string date)
     {
         _date = date;
+        this.LoadPrompts();
     }
 
 
     /* Methods */
+
+    public void LoadPrompts()
+    {
+        _prompts = new List<string>();
+        try
+        {
+            try{
+                
+                using(StreamReader reader = new StreamReader(_promptFilename))
+                {
+                    int lineCount = File.ReadLines(_promptFilename).Count();
+                    for (int i = 0; i <= lineCount; i++)
+                    {
+                        string csvLine = reader.ReadLine();
+                        string[] prompts = csvLine.Split(","); 
+                        foreach (string prompt in prompts)
+                        {
+                            _prompts.Add(prompt);
+                        }
+                    }
+                }
+                if (_prompts.Count == 0)
+                {
+                    _prompts = new List<string>
+                    {
+                        "How have I seen the hand of the Lord in my life today?",
+                        "What is something cool that happened today?",
+                        "What is something I am grateful for?",
+                        "If I had one thing I could do over today, what would it be?",
+                        "What am I looking forward to tomorrow?",
+                        "What was the strongest emotion I felt today?"
+                    };
+                    Console.WriteLine("Test 1");
+                }
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("This exception was hit");
+                _prompts = new List<string>
+                    {
+                        "How have I seen the hand of the Lord in my life today?",
+                        "What is something cool that happened today?",
+                        "What is something I am grateful for?",
+                        "If I had one thing I could do over today, what would it be?",
+                        "What am I looking forward to tomorrow?",
+                        "What was the strongest emotion I felt today?"
+                    };
+            }
+        }
+        catch (FileNotFoundException)
+        {
+             using(File.Create(_promptFilename)){}
+             _prompts = new List<string>
+                {
+                    "How have I seen the hand of the Lord in my life today?",
+                    "What is something cool that happened today?",
+                    "What is something I am grateful for?",
+                    "If I had one thing I could do over today, what would it be?",
+                    "What am I looking forward to tomorrow?",
+                    "What was the strongest emotion I felt today?"
+                };
+            
+        }
+    }
 
     // Converts the Entry into a CSV friendly string that can be processed.
     public string toCSV()
@@ -51,9 +113,15 @@ public class Entry
     // enter in their response.
     public void DisplayPrompt()
     {
-        _count = new Random().Next(0,3);
+        if (_prompts.Count == 0)
+        {
+            this.LoadPrompts();
+            Console.Write("This if loops was hit.");
+        }
+        Console.Write(_prompts.Count);
+        _count = new Random().Next(0,_prompts.Count);
         Console.Write(_date+": ");
-        Console.WriteLine(_prompts[_count]);
+        Console.WriteLine(_prompts[_count]+"\n>");
         string response = Console.ReadLine(); 
 
         // This part right here takes into account the fact that CSV files use commas
