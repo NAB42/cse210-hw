f185b5fbc72a02480c8241287166b2f0f6bfef5c
NAB42 <fj45cruza@gmail.com>
2026-05-15 16:28:32 -0600

    Now allows the user to choose a book and scripture

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,8 +5,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("> ");
-        Scripture s = new Scripture(Console.ReadLine(),3);
-        s.Write();
+        Console.Clear();
+        Console.WriteLine("Welcome to the Scripture Memorizer!");
+        while(true){
+            Console.Write(
+                """
+                Pick a book from the standard works (1-5):
+                1. Old Testament
+                2. New Testament
+                3. Book of Mormon
+                4. Doctrine and Covenants
+                5. Pearl of Great Price
+                ❯ 
+                """
+            );
+            int index = int.Parse(Console.ReadLine());
+            Console.Write("❯ ");
+            Scripture s = new Scripture(Console.ReadLine(),index);
+            s.Write();
+            Console.ReadLine();
+            Console.Clear();
+        }
     }
 }
