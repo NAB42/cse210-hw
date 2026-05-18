594e9fecbbdf034f269943d7dc228394b6436fc6
NAB42 <fj45cruza@gmail.com>
2026-05-18 14:29:48 -0600

    Created the Angle project

---
+++ sandbox/Angle/Angle.cs
@@ -0,0 +1,18 @@
+public class Angle
+{
+    private double _radians;
+
+    public Angle(double radians)
+    {
+        _radians=radians;
+    }
+
+    public double GetRadians()
+    {
+        return _radians;
+    }
+    public void SetRadians(double radians)
+    {
+        _radians=radians;
+    }
+}
