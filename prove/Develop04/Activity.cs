fb0ae77b88fa8bb16288c353ca19676f5321ce3c
NAB42 <fj45cruza@gmail.com>
2026-05-21 16:26:46 -0600

    Updated something

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,24 @@
+public class Activity
+{
+    /* Attributes */
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+
+    /* Constructors */
+    public Activity(string name,string description)
+    {
+        _name=name;
+        _description=description;
+    }
+
+    /* Methods */
+    public string GetInfo()
+    {
+        return $"{_name} Activity:\n{_description}";
+    }
+    public void SetDuration(int seconds)
+    {
+        _duration=seconds;
+    }
+}
