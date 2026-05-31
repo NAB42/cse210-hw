581d1c710273ca774359722d9821fdbdb5f27c9d
NAB42 <fj45cruza@gmail.com>
2026-05-30 22:22:42 -0600

    Updated things (realized I never pushed changes from days ago)

---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,10 @@
+public class ListingActivity : Activity 
+{
+	
+	public ListingActivity() : 
+		base("Listing Activity",
+			 "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.")
+	{
+
+	}
+}
