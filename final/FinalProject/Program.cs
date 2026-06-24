553e455486f2ba5cd2a22989932f80a55f7112a4
NAB42 <fj45cruza@gmail.com>
2026-06-24 16:38:54 -0600

    Started repeating display

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -6,6 +6,13 @@ class Program
     {
         Console.WriteLine("Note: if your display does not support unicode, this could be a very sad game.");
 		Board b = new Board();
-		b.Display();
+		Thread.Sleep(3000);
+		Console.CursorVisible = false;
+		while(true)
+		{
+			Console.Clear();
+			b.Display();
+			Thread.Sleep(10);
+		}
     }
 }
